63e653e7bb8117feb56b0e24188feb640a18e05f
civilKaos <m.glaser82@gmail.com>
2017-10-17 16:33:00 -0700

    Adds a welcome message to the page that prints out in the html. Includes a popup saying 'Hello'. Outputs messages to the console saying 'It Worked'

---
+++ script.js
@@ -0,0 +1,6 @@
+var greeting = "Welcome to my intro site. This greeting was saved as a variable in javascript and wrote to the page using \"document.write(greeting)\"";
+document.write(greeting);
+alert("Hello!");
+console.log("%cIt Worked","color:green;")
+console.warn("%cThis is a warning. Please check.","background:gold;");
+console.error("%cThis is an ERROR!!!","color:red;background:pink;");
